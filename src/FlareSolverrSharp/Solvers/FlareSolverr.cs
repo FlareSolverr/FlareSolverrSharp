d6df39d35f75325c8b9f634203eaa607591d8c84
ngosang <ngosang@hotmail.es>
2023-01-05 13:19:43 +0100

    Fix Return FlareSolverr timeout message

--- src/FlareSolverrSharp/Solvers/FlareSolverr.cs
+++ src/FlareSolverrSharp/Solvers/FlareSolverr.cs
@@ -76,8 +76,8 @@ namespace FlareSolverrSharp.Solvers
                 try
                 {
                     _httpClient = new HttpClient();
-                    // wait 2 more seconds to make sure we return the FlareSolverr timeout message
-                    _httpClient.Timeout = TimeSpan.FromMilliseconds(MaxTimeout + 2000);
+                    // wait 5 more seconds to make sure we return the FlareSolverr timeout message
+                    _httpClient.Timeout = TimeSpan.FromMilliseconds(MaxTimeout + 5000);
                     response = await _httpClient.PostAsync(_flareSolverrUri, flareSolverrRequest);
                 }
                 catch (HttpRequestException e)
